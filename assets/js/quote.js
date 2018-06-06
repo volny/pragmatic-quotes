eaecd1a1f461cbc294b96a2b6dd3e51017609065
Felix Volny <github@volny.co>
2018-06-06 23:42:56 +0700

    tweet intent like twitter tells me to

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -54,11 +54,7 @@
 
   function updateTwitterButton(title, body) {
     var href= makeTwitterLink(makeTweet(title, body, getHash()));
-    $('#tweetButton').click(function() {
-      console.log(href)
-      // TODO this better be a modal
-      window.open(href, '_blank');
-    })
+    $('#tweetLink').attr('href', href);
   }
 
   function injectNew(data, id) {
@@ -103,54 +99,4 @@
       }, 1000)
     });
   });
-
-  /*
-   * TWITTER
-   */
-
-  // twitter intent code (https://dev.twitter.com/web/intents)
-  (function() {
-    if (window.__twitterIntentHandler) return;
-    var intentRegex = /twitter\.com\/intent\/(\w+)/,
-        windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
-        width = 550,
-        height = 253,
-        winHeight = screen.height,
-        winWidth = screen.width;
-
-    function handleIntent(e) {
-      e = e || window.event;
-      var target = e.target || e.srcElement,
-          m, left, top;
-
-      while (target && target.nodeName.toLowerCase() !== 'a') {
-        target = target.parentNode;
-      }
-
-      if (target && target.nodeName.toLowerCase() === 'a' && target.href) {
-        m = target.href.match(intentRegex);
-        if (m) {
-          left = Math.round((winWidth / 2) - (width / 2));
-          top = 0;
-
-          if (winHeight > height) {
-            top = Math.round((winHeight / 2) - (height / 2));
-          }
-
-          window.open(target.href, 'intent', windowOptions + ',width=' + width +
-                                             ',height=' + height + ',left=' + left + ',top=' + top);
-          e.returnValue = false;
-          e.preventDefault && e.preventDefault();
-        }
-      }
-    }
-
-    if (document.addEventListener) {
-      document.addEventListener('click', handleIntent, false);
-    } else if (document.attachEvent) {
-      document.attachEvent('onclick', handleIntent);
-    }
-    window.__twitterIntentHandler = true;
-  }());
-
 })(jQuery)
